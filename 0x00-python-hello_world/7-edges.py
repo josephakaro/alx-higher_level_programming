b31c3d29edb1ae696a007c34fed6a61833519ede
josephmarko <josephmmakaro@gmail.com>
2023-09-04 20:29:08 +0000

    Finist edges

--- 0x00-python-hello_world/7-edges.py
+++ 0x00-python-hello_world/7-edges.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 word = "Holberton"
-# YOUR CODE GOES HERE. PLEASE REMOVE THIS LINE
+word_first_3 = word[0:3]
+word_last_2 = word[7:9]
+middle_word = word[1:-1]
 print(f"First 3 letters: {word_first_3}")
 print(f"Last 2 letters: {word_last_2}")
 print(f"Middle word: {middle_word}")
