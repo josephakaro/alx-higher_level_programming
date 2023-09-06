2d092e85e9b5f58e781c21764b667a5f0efdbe04
josephmarko <josephmmakaro@gmail.com>
2023-09-06 00:33:56 +0000

    Advance Files

---
+++ 0x01-python-if_else_loops_functions/102-magic_calculation.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def magic_calculation(a, b, c):
+    while a and b and c:
+        if a < b:
+            return (c)
+        
+        if c > b:
+            return (a + b)
+        return (a * b - c)
