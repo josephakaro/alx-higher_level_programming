6413337e52ba67007ae55008f6098f4926e34de1
Joseph Marko <josephmmakaro@gmail.com>
2023-09-07 21:59:47 +0000

    Compleeted ✅

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -2,9 +2,9 @@
 
 if __name__ == "__main__":
     """Print all the names called by hidden_4 module."""
-    import hidden_4
+    import hidden_4 as names
 
-    hidden_names = dir(hidden_4)
+    hidden_names = dir(names)
     for name in hidden_names:
         if name[:2] != "__":
             print(hidden_names)
--- 0x02-python-import_modules/5-variable_load.py
+++ 0x02-python-import_modules/5-variable_load.py
@@ -2,5 +2,5 @@
 
 if __name__ == "__main__":
     """Print the value of variable found in variable_load_5 Module."""
-    from variable_load_5 import a
-    print(a)
+    from variable_load_5 import a as variables
+    print(variables)
