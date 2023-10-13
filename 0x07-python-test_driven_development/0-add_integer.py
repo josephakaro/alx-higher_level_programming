9735bb41008f335f78b71723f6cef27d197b6251
Joseph Marko <josephmmakaro@gmail.com>
2023-10-13 19:06:25 +0000

    Test 1

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,12 +1,17 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 """ Function that adds 2 integers """
 
+
+
 def add_integer(a, b=98):
-    """ Function that adds 2 integers """
+    """A function that adds 2 integers"""
     if type(a) not in [int, float]:
         raise TypeError('a must be an integer')
-
     if type(b) not in [int, float]:
         raise TypeError('b must be an integer')
-    
-    return (int(a) + int(b))
+    return int(a) + int(b)
+
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testfile('tests/0-add_integer.txt')
