24cf94e6f6a775ae631fbcff05dacd2a448b9d5c
Joseph Marko <josephmmakaro@gmail.com>
2023-10-14 15:24:10 +0000

    0-Task, Bug-0

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -4,7 +4,7 @@
 
 
 def add_integer(a, b=98):
-    """A function that adds 2 integers"""
+    """Function that adds two integers a and b."""
     if type(a) not in [int, float]:
         raise TypeError('a must be an integer')
     if type(b) not in [int, float]:
