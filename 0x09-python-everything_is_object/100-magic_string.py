63e908be3cfe78551eea99efe9978acc658b2555
Joseph Marko <josephmmakaro@gmail.com>
2023-10-20 12:42:22 +0000

    New Function Updated

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,5 +1,3 @@
 #!/usr/bin/python3
-def magic_string():
-    for n in range(n):
-        print("BestSchool")
-    print("BestSchool, ")
+def magic_string(n):
+   return ''.join(['BestSchool' for _ in range(n)])
