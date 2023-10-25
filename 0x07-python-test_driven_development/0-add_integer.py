ccfe7d4bf27daff9715877d656785d4d0ec1c323
Joseph Marko <josephmmakaro@gmail.com>
2023-10-25 18:53:16 +0000

    Corrected Task 0

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -13,5 +13,5 @@ def add_integer(a, b=98):
 
 
 if __name__ == "__main__":
-    import doctest
-    doctest.testfile('tests/0-add_integer.txt')
+	import doctest
+	doctest.testfile('tests/0-add_integer.txt')
