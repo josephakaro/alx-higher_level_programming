c4e0555d84d6c30c37c734dce4d6ded03c20b7fb
Joseph Marko <josephmmakaro@gmail.com>
2023-12-11 20:06:41 +0000

    Fixes

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -2,7 +2,7 @@
 const { argv } = require("node:process");
 
 if (argv.length >= 3) {
-  console.log("Argument found");
+  console.log("Arguments found");
 } else if (argv.length <= 2) {
-  console.log("No arguments");
+  console.log("No argument");
 }
