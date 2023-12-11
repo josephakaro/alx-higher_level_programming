8914401443a1e4b18a7b6a7f60ba1e058ecd20a7
Joseph Marko <josephmmakaro@gmail.com>
2023-12-11 17:49:44 +0000

    Cleaned

--- 0x12-javascript-warm_up/0-javascript_is_amazing.js
+++ 0x12-javascript-warm_up/0-javascript_is_amazing.js
@@ -1,4 +1,3 @@
 #!/usr/bin/node
-/* A script that prints 'JavaScript is Amazing'*/
 const myVar = "JavaScript is amazing";
-if (myVar) console.log(myVar);
+console.log(myVar);
--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,3 +1,3 @@
 #!/usr/bin/node
 const lines = "C is fun\nPython is cool\nJavaScript amazing";
-if (lines) console.log(lines);
+console.log(lines);
