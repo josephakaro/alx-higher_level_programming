60ea117b9cb4b8050525b8a25baad1a90077d89e
Joseph Marko <josephmmakaro@gmail.com>
2023-12-11 22:31:10 +0000

    Completed, bugs unfixed

--- 0x12-javascript-warm_up/0-javascript_is_amazing.js
+++ 0x12-javascript-warm_up/0-javascript_is_amazing.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-// Print string “Javascript is amazing”
+// Script that prints “JavaScript is amazing”
 
 const myVar = "Javascript is amazing";
 console.log(myVar);
---
+++ 0x12-javascript-warm_up/100-let_me_const.js
@@ -0,0 +1,4 @@
+#!/usr/bin/node
+// Modifies the value of myVar to 333
+
+myVar = 333;
