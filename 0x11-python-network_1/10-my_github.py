f1dbf843373d1f8e2406fe154cabca741b908c47
Joseph Akaro <josephmmakaro@gmail.com>
2024-01-14 14:53:19 +0000

    completed🛴

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -14,12 +14,13 @@ def main(argv):
     """
     user = argv[1]
     password = argv[2]
-    response = requests.get("https://api.github.com/user",
+    response = requests.get('https://api.github.com/user',
                             auth=HTTPBasicAuth(user, password))
     try:
         profile_info = response.json()
-        print(profile_info['id'])
-    except TypeError:
+        if profile_info.id == id:
+            print(profile_info['id'])
+    except ValueError:
         print('None')
 
 
