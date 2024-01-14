d124ce31c7828e1dc015df4551a98f860417b4c7
Joseph Akaro <josephmmakaro@gmail.com>
2024-01-14 13:40:19 +0000

    4-header

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -2,4 +2,4 @@
 # URL input
 URL= $1
 # Script that takes in a URL as an argument, sends a GET request to the URL, and displays the body of the response, A header variable X-HolbertonSchool-User-Id must be sent with the value 98
-curl -H "X-HolbertonSchool-User-Id: 98" -X GET $URL
+curl -s $URL -H "X-HolbertonSchool-User-Id: 98" -X GET $URL
