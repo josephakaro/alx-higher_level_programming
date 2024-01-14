b5460b7644f33302af25140fb16b0c781e5d39ca
Joseph Akaro <josephmmakaro@gmail.com>
2024-01-14 15:19:33 +0000

    Semistandard --fix

--- 0x13-javascript_objects_scopes_closures/10-converter.js
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 
 exports.converter = function (base) {
-    return function (num) {
-      return num.toString(base);
-    };
+  return function (num) {
+    return num.toString(base);
   };
+};
