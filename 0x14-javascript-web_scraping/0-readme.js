3da60a4a265395cb31c148b9cc1bd8bc290addd9
Joseph Akaro <josephmmakaro@gmail.com>
2024-02-08 20:32:32 +0000

    completed

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -2,9 +2,9 @@
 const fs = require('fs');
 const file = process.argv[2];
 fs.readFile(file, 'utf-8', function (err, data) {
-	if (err) {
-		console.log(err);
-	} else {
-		console.log(data);
-	}
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(data);
+  }
 });
